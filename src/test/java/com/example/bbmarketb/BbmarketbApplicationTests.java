8f83df799581e9614e67368e2ba3843096c08050
백승재 <zaxw5965@naver.com>
2025-06-28 00:58:59 +0900

    Project Start

---
+++ src/test/java/com/example/bbmarketb/BbmarketbApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.bbmarketb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbmarketbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    void asd(){
+        System.out.println("1");
+    }
+
+}
